2d7fe7cca426e768cdca4a8a5c9899d3fb8f2406
GoZavCode <mgonaid7@gmail.com>
2025-03-20 13:25:57 +0100

    Tilføjede attributter og getters for order klassen.

--- src/Order.java
+++ src/Order.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+
+
 public class Order {
+    private ArrayList<Pizza> pizzaList = new ArrayList<>();
+    private int orderIDCounter;
+    private static int orderID;
+    private int deliveryTimeHours;
+    private int deliveryTimeMinutes;
+
+public int getOrderID() {
+    return orderID;
+}
+public int getDeliveryTimeHours()  {
+    return deliveryTimeHours;
+}
+public int getDeliveryTimeMinutes() {
+    return deliveryTimeMinutes;
+}
 }
---
+++ src/PizzaMenu.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class PizzaMenu {
+    private ArrayList<Pizza> pizzaList = new ArrayList<>();
+
+
+}
