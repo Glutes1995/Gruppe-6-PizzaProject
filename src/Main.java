4b24808780e1a95e4367c11da4e544401fcaa16b
Glutes1995 <jynke58@hotmail.com>
2025-03-21 11:36:47 +0100

    Arbejder på menukortet

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,7 @@ public class Main {
         //Man kan bare slette hvad end der er her i main og teste det man nu skal have testet"
 
         PizzaMenu pizzaMenu = new PizzaMenu();
-
-        pizzaMenu.addPizza("Margarita",49,1,"Tomato sauce, Cheese");
+        pizzaMenu.addPizzaMenu(pizzaMenu);
         System.out.println(pizzaMenu);
-
-        pizzaMenu.removePizza(1);
-        System.out.println(pizzaMenu);
-
-        Pizza pizza = new Pizza("Meat lover", 69, 2);
-        System.out.println(pizza);
     }
 }
--- src/PizzaMenu.java
+++ src/PizzaMenu.java
@@ -4,6 +4,27 @@ public class PizzaMenu {
     //PizzaMenu instansvariabel
     private ArrayList<Pizza> pizzaMenu = new ArrayList<>();
 
+    //Metoden tilføjer alle pizzaerne på menukortet til et PizzaMenu objekt
+    public void addPizzaMenu(PizzaMenu pizzaMenu) {
+        pizzaMenu.addPizza("Margarita",49,1,"Tomato sauce, Cheese");
+        pizzaMenu.addPizza("Hawaii", 55, 2,"Tomato sauce, Cheese, Ham, Pineapple");
+        pizzaMenu.addPizza("Pep dress", 55, 3,"Tomato sauce, Cheese, Pepperoni, Dressing");
+        pizzaMenu.addPizza("Marios recommended",60, 4,"Tomato sauce, Cheese, Ham, Pepperoni");
+        pizzaMenu.addPizza("Meat lover", 69, 5, "Tomato sauce, Cheese, Ham, Bacon, Pepperoni, Beef, Chicken");
+        pizzaMenu.addPizza("Kea special", 75, 6,"Tomato sauce, Cheese, Ham, Bacon, Pepperoni, Dressing");
+        pizzaMenu.addPizza("Giga chad", 75, 7,"Tomato sauce, Cheese,");
+        pizzaMenu.addPizza("The rizzler", 75, 8,"Tomato sauce, Cheese,");
+        pizzaMenu.addPizza("Hangover", 75, 9,"Tomato sauce, Cheese,");
+        pizzaMenu.addPizza("Worse hangover", 80, 10,"Tomato sauce, Cheese,");
+        pizzaMenu.addPizza("worst hangover", 85, 11,"Tomato sauce, Cheese,");
+        pizzaMenu.addPizza("Pineapple lover", 90, 12,"Tomato sauce, Cheese,");
+        pizzaMenu.addPizza("Bacon paradise", 95, 13,"Tomato sauce, Cheese,");
+        pizzaMenu.addPizza("Potato dream", 95, 14,"Tomato sauce, Cheese,");
+        pizzaMenu.addPizza("The spicy one", 99, 15,"Tomato sauce, Cheese,");
+        pizzaMenu.addPizza("Mystery pizza", 102, 16,"Tomato sauce, Cheese,");
+
+    }
+
     //metode til at tilføje pizzaer til menukortet
     public void addPizza(String name, int price, int pizzaNumber, String topping) {
         Pizza pizza = new Pizza(name, price, pizzaNumber, topping);
