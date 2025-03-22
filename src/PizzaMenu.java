28f31ec3802d6e16312fa3ce74fe0f74ec430fec
Glutes1995 <jynke58@hotmail.com>
2025-03-22 13:00:29 +0100

    Arbejder på menukortet

--- src/PizzaMenu.java
+++ src/PizzaMenu.java
@@ -13,30 +13,30 @@ public class PizzaMenu {
         pizzaMenu.addPizza("Meat lover", 69, 5, "Tomato sauce, Cheese, Ham, Bacon, Pepperoni, Beef, Chicken");
         pizzaMenu.addPizza("Kea special", 75, 6,"Tomato sauce, Cheese, Ham, Bacon, Pepperoni, Dressing");
         pizzaMenu.addPizza("Giga chad", 75, 7,"Tomato sauce, Cheese, flank steak, parmigiano");
-        pizzaMenu.addPizza("The rizzler", 75, 8,"Tomato sauce, Cheese,");
-        pizzaMenu.addPizza("Hangover", 75, 9,"Tomato sauce, Cheese,");
-        pizzaMenu.addPizza("Worse hangover", 80, 10,"Tomato sauce, Cheese,");
-        pizzaMenu.addPizza("worst hangover", 85, 11,"Tomato sauce, Cheese,");
-        pizzaMenu.addPizza("Pineapple lover", 90, 12,"Tomato sauce, Cheese,");
-        pizzaMenu.addPizza("Bacon paradise", 95, 13,"Tomato sauce, Cheese,");
+        pizzaMenu.addPizza("The rizzler", 75, 8,"Tomato sauce, Cheese, Chicken, Bacon, Pepperoni");
+        pizzaMenu.addPizza("Hangover", 75, 9,"Tomato sauce, Cheese, Ham, Bacon, Pepperoni, Dressing");
+        pizzaMenu.addPizza("Worse hangover", 80, 10,"Tomato sauce, Cheese, Ham, Bacon, Pepperoni, Chicken, Dressing");
+        pizzaMenu.addPizza("worst hangover", 85, 11,"Tomato sauce, Cheese, Extra cheese, Pepperoni, Dressing");
+        pizzaMenu.addPizza("Pineapple lover", 90, 12,"Tomato sauce, Cheese, Pepperoni, Pineapple, Chili");
+        pizzaMenu.addPizza("Bacon paradise", 95, 13,"Tomato sauce, Cheese, Bacon, Mushroom, Garlic");
         pizzaMenu.addPizza("Potato dream", 95, 14,"Tomato sauce, Cheese, Potato, Bacon, Rosemary");
         pizzaMenu.addPizza("The spicy one", 99, 15,"Tomato sauce, Cheese, Ham, Pepperoni, chili");
-        pizzaMenu.addPizza("Mystery pizza", 102, 16,"Tomato sauce, Cheese,");
-        pizzaMenu.addPizza("4 cheeses",100, 17,"Tomato sauce, Cheese,");
-        pizzaMenu.addPizza("Trouble truffel",100, 18,"Tomato sauce, Cheese,");
+        pizzaMenu.addPizza("Mystery pizza", 102, 16,"Tomato sauce, Cheese, + Chefs choise");
+        pizzaMenu.addPizza("4 cheeses",100, 17,"Tomato sauce, Cheese, Emmental, Parmigiano, Gorgonzola");
+        pizzaMenu.addPizza("Trouble truffel",100, 18,"Tomato sauce, Cheese, Ham, Mushroom, Onion, Truffle");
         pizzaMenu.addPizza("Garlic",80, 19,"Tomato sauce, Cheese, Ham, Garlic");
-        pizzaMenu.addPizza("Mushroom",100, 20,"Tomato sauce, Cheese, Ham, Mushroom,");
-        pizzaMenu.addPizza("Veggie",100, 21,"Tomato sauce, Cheese,");
-        pizzaMenu.addPizza("Vegan special",100, 22,"Tomato sauce, Cheese,");
-        pizzaMenu.addPizza("Vegan tropical",100, 23,"Tomato sauce, Cheese,");
-        pizzaMenu.addPizza("Gluten free",100, 24,"Tomato sauce, Cheese,");
+        pizzaMenu.addPizza("Mushroom",100, 20,"Tomato sauce, Cheese, Ham, Mushroom");
+        pizzaMenu.addPizza("Veggie",100, 21,"Tomato sauce, Cheese, Eggplant, Zucchini, Sundried tomato, Spinach");
+        pizzaMenu.addPizza("Vegan special",100, 22,"Tomato sauce, Eggplant, Mushroom, Zucchini, Fresh tomato, Spinach ");
+        pizzaMenu.addPizza("Vegan tropical",100, 23,"Tomato sauce, Cheese, Peppers, Zucchini, Pineapple, Garlic");
+        pizzaMenu.addPizza("Gluten free",100, 24,"Tomato sauce, Cheese, Ham, Pepperoni, Onion");
         pizzaMenu.addPizza("August special",100, 25,"Tomato sauce, Cheese,");
         pizzaMenu.addPizza("Joshua special",100, 26,"Tomato sauce, Cheese,");
         pizzaMenu.addPizza("Simon special",100, 27,"Tomato sauce, Cheese,");
         pizzaMenu.addPizza("Martin special",100, 28,"Tomato sauce, Cheese,");
         pizzaMenu.addPizza("Group 6 special",100, 29,"Tomato sauce, Cheese,");
-        pizzaMenu.addPizza("The indian",100, 30,"Tomato sauce, Cheese,");
-        pizzaMenu.addPizza("Manhatten",100, 31,"Tomato sauce, Cheese,");
+        pizzaMenu.addPizza("The indian",100, 30,"Tomato sauce, Cheese, Butter chicken, Cashew sauce,");
+        pizzaMenu.addPizza("Manhatten",100, 31,"Tomato sauce, Cheese, Beef, Bacon, Pepperoni");
     }
 
     //metode til at tilføje pizzaer til menukortet
