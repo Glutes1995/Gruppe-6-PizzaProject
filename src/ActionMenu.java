cc43c25f4ec434e33261dedc3a2c32fa9c790bcd
SimAbild <abildtrup123@gmail.com>
2025-03-24 11:09:11 +0100

    Lavet en Switch menu

---
+++ src/ActionMenu.java
@@ -0,0 +1,12 @@
+public class ActionMenu {
+
+    //Metode til at vise handlinger
+    public static void visActions() {
+        System.out.println("Hvad vil du foretage dig?");
+        System.out.println("1 - Se menukortet");
+        System.out.println("2 - Lav en bestilling");
+        System.out.println("3 - Ændre pris på pizza");
+        System.out.println("4 - Se dagens bestillinger");
+        System.out.println("5 - Afslut programmet");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,80 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Test features manuelt her inde\n");
+        //For eksempel har jeg testet ting, der skal vise noget
+        //Man kan bare slette hvad end der er her i main og teste det man nu skal have testet"
+
+        PizzaMenu pizzaMenu = new PizzaMenu();
+        Scanner scanner = new Scanner(System.in);
+        boolean actionMenu = true;
+
+        pizzaMenu.addPizza("Margarita",49,1,"Tomato sauce, Cheese");
+        System.out.println(pizzaMenu);
+
+        pizzaMenu.removePizza(1);
+        System.out.println(pizzaMenu);
+
+        Pizza pizza = new Pizza("Meat lover", 69, 2);
+        System.out.println(pizza);
+
+        //Test OrderList og Order klasserne
+        pizzaMenu.addPizza("Margarita",49,1,"Tomato sauce, Cheese");
+        System.out.println(pizzaMenu);
+
+        OrderList orderList = new OrderList();
+
+        orderList.addOrder(pizzaMenu, scanner);
+        System.out.println("OrderList1: ");
+        System.out.println(orderList);
+
+        orderList.addOrder(pizzaMenu, scanner);
+        System.out.println("OrderList2: ");
+        System.out.println(orderList);
+
+        orderList.addOrder(pizzaMenu, scanner);
+        System.out.println("OrderList3: ");
+        System.out.println(orderList);
+
+        while(actionMenu){
+            //Viser ordrehistorikken
+            System.out.println(orderList);
+
+            //Viser handlingsmenuen
+            ActionMenu.visActions();
+
+            //Læser brugerens input
+            String actions = scanner.nextLine();
+
+            switch (actions){
+                case "1": //Viser pizza menu
+                    System.out.println(pizzaMenu);
+                    break;
+
+                case "2": //Lav bestilling
+                    orderList.addOrder(pizzaMenu, scanner);
+                    break;
+
+                case "3": //Ændre en pris på pizza
+
+                    break;
+
+                case "4": //
+
+                    break;
+
+                case "5": //Afslutter programmet
+                    System.out.println("Programmet afsluttes");
+                    actionMenu = false;
+                    break;
+
+                default: //Hvis brugeren indtaster ugyldigt valg
+                    System.out.println("Indtast et gyldigt valg");
+
+            }
+        }
     }
 }
