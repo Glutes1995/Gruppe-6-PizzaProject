2e777e9ef1b250a557cbdaffaff37e9d862cc7d6
SimAbild <abildtrup123@gmail.com>
2025-03-24 11:20:41 +0100

    Lavet en Switch menu

--- src/Main.java
+++ src/Main.java
@@ -4,40 +4,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test features manuelt her inde\n");
-        //For eksempel har jeg testet ting, der skal vise noget
-        //Man kan bare slette hvad end der er her i main og teste det man nu skal have testet"
-
+        //Variabler
         PizzaMenu pizzaMenu = new PizzaMenu();
         Scanner scanner = new Scanner(System.in);
         boolean actionMenu = true;
-
-        pizzaMenu.addPizza("Margarita",49,1,"Tomato sauce, Cheese");
-        System.out.println(pizzaMenu);
-
-        pizzaMenu.removePizza(1);
-        System.out.println(pizzaMenu);
-
-        Pizza pizza = new Pizza("Meat lover", 69, 2);
-        System.out.println(pizza);
-
-        //Test OrderList og Order klasserne
-        pizzaMenu.addPizza("Margarita",49,1,"Tomato sauce, Cheese");
-        System.out.println(pizzaMenu);
-
         OrderList orderList = new OrderList();
-
-        orderList.addOrder(pizzaMenu, scanner);
-        System.out.println("OrderList1: ");
-        System.out.println(orderList);
-
-        orderList.addOrder(pizzaMenu, scanner);
-        System.out.println("OrderList2: ");
-        System.out.println(orderList);
-
-        orderList.addOrder(pizzaMenu, scanner);
-        System.out.println("OrderList3: ");
-        System.out.println(orderList);
 
         while(actionMenu){
             //Viser ordrehistorikken
@@ -51,10 +22,11 @@ public class Main {
 
             switch (actions){
                 case "1": //Viser pizza menu
-                    System.out.println(pizzaMenu);
+                    pizzaMenu.getPizzaMenu();
                     break;
 
                 case "2": //Lav bestilling
+                    System.out.println(pizzaMenu);
                     orderList.addOrder(pizzaMenu, scanner);
                     break;
 
