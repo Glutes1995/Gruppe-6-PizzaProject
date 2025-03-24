7b98e2656aac83b6910a9bd9ee91fb9fabf7d408
Glutes1995 <jynke58@hotmail.com>
2025-03-24 10:37:27 +0100

    Arbejder på menukortet

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test features manuelt her inde\n");
-        //For eksempel har jeg testet ting, der skal vise noget
-        //Man kan bare slette hvad end der er her i main og teste det man nu skal have testet"
-
-        PizzaMenu pizzaMenu = new PizzaMenu();
-        pizzaMenu.addPizzaMenu(pizzaMenu);
-        System.out.println(pizzaMenu);
-
     }
 }
