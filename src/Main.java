cb487aa29fd9313b5935f9a62a96637b84515ded
Lornst <augustgosden@gmail.com>
2025-03-26 12:00:02 +0100

    CodeWithMe v2 testMetode if statement

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,17 @@ public class Main {
         System.out.println("v5");
         System.out.println("v6");System.out.println("v4");
         System.out.println("v5");
-        System.out.println("v6");System.out.println("v4");
-        System.out.println("v5");
-        System.out.println("v6");System.out.println("v4");
-        System.out.println("v5");
-        System.out.println("v6");System.out.println("v4");
-        System.out.println("v5");
-        System.out.println("v6");System.out.println("v4");
-        System.out.println("v5");
-        System.out.println("v6");System.out.println("v4");
+        if(true){
+            System.out.println("v6");System.out.println("v4");
+            System.out.println("v5");
+            System.out.println("v6");System.out.println("v4");
+            System.out.println("v5");
+            System.out.println("v6");System.out.println("v4");
+            System.out.println("v5");
+            System.out.println("v6");System.out.println("v4");
+            System.out.println("v5");
+            System.out.println("v6");System.out.println("v4");
+        }
         System.out.println("v5");
         System.out.println("v6");System.out.println("v4");
         System.out.println("v5");
