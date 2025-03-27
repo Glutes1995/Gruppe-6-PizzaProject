3a1f19d31407cbd97ee1b937b7d41f6f8d23b48f
Lornst <augustgosden@gmail.com>
2025-03-27 13:18:21 +0100

    addOrderDeliveryTime metode safeguards

--- src/Order.java
+++ src/Order.java
@@ -30,13 +30,28 @@ public class Order {
 
     void addOrderDeliveryTime(Scanner scanner) {
         String input;
+        boolean incorrectInput = false;
 
-        System.out.println("Skriv leverings tid som 00:00 timer:minutter");
-        input = scanner.nextLine();
+        do{
+            System.out.println("Skriv leverings tid som 00:00 timer:minutter");
+            input = scanner.nextLine();
 
-        String[] timeSplit = input.split(":");
-        deliveryTimeHours = Integer.parseInt(timeSplit[0]);
-        deliveryTimeMinutes = Integer.parseInt(timeSplit[1]);
+            try{
+                String[] timeSplit = input.split(":");
+
+                deliveryTimeHours = Integer.parseInt(timeSplit[0]);
+                deliveryTimeMinutes = Integer.parseInt(timeSplit[1]);
+                if(deliveryTimeHours > 23 || deliveryTimeMinutes > 59 || deliveryTimeHours < 0 || deliveryTimeMinutes < 0){
+                    System.out.println("Ugyldigt afhentningstidspunkt");
+                    incorrectInput = true;
+                }else{
+                    incorrectInput = false;
+                }
+            }catch(NumberFormatException e){
+                System.out.println("Ugyldigt afhentningstidspunkt");
+                incorrectInput = true;
+            }
+        }while(incorrectInput);
     }
 
     //addPizza metoden tager den pizzamenuen som parametrer for at tilføje pizza'er fra den allerde etablerede menu
